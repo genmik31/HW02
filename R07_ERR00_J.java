a6983138ba11831b39996d4c7240cb030bf9dc90
genmik31 <mlabarbera6@gmail.com>
2025-10-04 17:15:32 -0400

    fix ERR00-J: handle or propagate checked exception

--- R07_ERR00_J.java
+++ R07_ERR00_J.java
@@ -1,5 +1,7 @@
 try {
     someApiThatThrows();
 } catch (IOException e) {
-    // ignored
+    // handle or propagate
+    System.err.println("I/O error: " + e.getMessage());
+    // or: throw e;  (rethrow after logging)
 }
